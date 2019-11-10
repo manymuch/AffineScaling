205e33dc30933123ef56e90c3df7f8f6bf34ad2c
manymuch <zjx219@gmail.com>
2019-11-10 16:01:58 -0500

    initial commit, create class

---
+++ AffineScaling.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+class AffineScaling():
+
+# Consider standard form:
+# min c'x
+# s.t. Ax = b, x>=0
+    def __init__(self, A,b,c,x):
+        m = A.shape()
+        print(m)
+
+
+
+
+if __name__ == "__main__":
+    
+    # LP problem in standard form
+    
